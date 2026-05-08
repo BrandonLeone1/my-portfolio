4bdbaf07435e0ad924c9a97d9fad0db84cd8bbf3
BrandonLeone1 <brandonjleone@gmail.com>
2026-05-08 09:30:54 -0400

    Added slight padding changes on scroll and changed skills section ID location to improve scrolling accuracy for the section.

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -1,8 +1,9 @@
 export default function SkillsSection () {
     return (
         <>
-        <div className="max-w-5xl mx-auto p-6" id="skills">
-            <p className="text-4xl mt-18 text-neutral-100 font-semibold">Skills</p>
+        <div className="max-w-5xl mx-auto p-6">
+            
+            <p id="skills" className="text-4xl mt-18 text-neutral-100 font-semibold">Skills</p>
             <p className="mt-6 text-neutral-100 text-xl font-medium">Some of the technologies I've been working with:</p>
         
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 p-6 mt-6 gap-6">
@@ -41,6 +42,8 @@ export default function SkillsSection () {
                 </div>
             </div>
         </div>
+        
+
         </>
     )
 }
